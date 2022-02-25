dd6c4f5a32127002f4e32f5aef783bcbee25835c
bhong92 <bhong1992@gmail.com>
2022-02-24 21:57:25 -0800

    20220224

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from scrapper import findElement, getJSON
 
 app = FastAPI()
 
-@app.post("/api/plot", response_class=UJSONResponse)
+@app.post("/api/chem-wiki", response_class=UJSONResponse)
 async def information(element: str):
     return (getJSON(element))
